5400ec84e12e0de8ee4a62564f55acea01b6bd7c
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2020-10-13 15:39:09 +0100

    full paths required in .env

--- workflow/scripts/rels/gene_to_protein.py
+++ workflow/scripts/rels/gene_to_protein.py
@@ -7,23 +7,27 @@ import pandas as pd
 
 #################### leave me heare please :) ########################
 
-from utils.general import setup
+from workflow.scripts.utils.general import setup
 
-from utils.writers import (
+from workflow.scripts.utils.writers import (
     create_constraints,
     create_import,
 )
 
-
-# setup and return path to attribute directory
-args, dataDir = setup()
+# setup 
+args, dataDir, dataFiles = setup()
 meta_id = args.name
+
+# args = the argparse arguements (name and data)
+# dataDir = the path to the working directory for this node/rel
+# dataFiles = dictionary of source files specified in data_integration.yml
 
 #######################################################################
 
 
 def gene_protein():
-    data = os.path.join(dataDir, "protein-data-sp.txt.gz")
+    FILE = os.path.basename(dataFiles['protein'])
+    data = os.path.join(dataDir, FILE)
     df = pd.read_csv(data, sep="\t")
     df.columns = ["chr", "source", "target"]
     df.drop(columns=["chr"], inplace=True)
