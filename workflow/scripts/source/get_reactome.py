89f28bc5e9f12a60b70d1bd63bf759e8c81c6abc
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2020-10-13 13:13:13 +0100

    biomart protein

--- workflow/scripts/source/get_reactome.py
+++ workflow/scripts/source/get_reactome.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import os 
+import datetime
+
+from workflow.scripts.utils import settings
+env_configs = settings.env_configs
+
+data_dir=os.path.join(env_configs['data_dir'],"reactome")
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+    
+today = datetime.date.today()
 
 def protein_to_pathway():
     #protein to pathway
@@ -14,34 +25,8 @@ def protein_to_pathway():
     ]
     df = df[df['species']=='Homo sapiens']
     print(df.head())
-    df.to_csv('UniProt2Reactome_All_Levels_human.csv',index=False)
+    df.to_csv(os.path.join(data_dir,f'UniProt2Reactome_All_Levels_human_{today}.csv'),index=False)
 
-def pathway_to_pmid():
-    #pathway to pmid
-    url="https://reactome.org/download/current/ReactionPMIDS.txt"
-    df=pd.read_csv(url,sep='\t')
-    df.columns=[
-        'reactome_id',
-        'pmid'
-    ]
-    print(df.head())
-    df.to_csv('ReactionPMIDS.csv',index=False)
-
-def id_to_reaction():
-    #identifier to reaction
-    url="https://reactome.org/download/current/UniProt2ReactomeReactions.txt"
-    df=pd.read_csv(url,sep='\t')
-    df.columns=[
-        'source_id',
-        'reactome_id',
-        'url',
-        'event',
-        'evidence_code',
-        'species'
-    ]
-    df = df[df['species']=='Homo sapiens']
-    print(df.head())
-    df.to_csv('UniProt2ReactomeReactions.csv',index=False)
 
 def pathways():
     #pathways
@@ -55,7 +40,7 @@ def pathways():
     ]
     df1 = df1[df1['species']=='Homo sapiens']
     print(df1.head())
-    df1.to_csv('ReactomePathways_human.csv',index=False)
+    df1.to_csv(os.path.join(data_dir,f'ReactomePathways_human_{today}.csv'),index=False)
 
     #hierarchy
     url="https://reactome.org/download/current/ReactomePathwaysRelation.txt"
@@ -68,16 +53,9 @@ def pathways():
     print(df2.shape)
     df2=df2[df2['parent'].isin(df1['reactome_id'])]
     print(df2.shape)
-    df2.to_csv('ReactomePathwaysRelation_human.csv',index=False)
+    df2.to_csv(os.path.join(data_dir,f'ReactomePathwaysRelation_human_{today}.csv'),index=False)
 
 
-    #complex to pathway
-    #url="https://reactome.org/download/current/Complex_2_Pathway_human.txt"
-
-def get_data():
-    #protein_to_pathway()
+if __name__ == "__main__":
+    protein_to_pathway()
     pathways()
-    #pathway_to_pmid()
-    #id_to_reaction()
-
-get_data()
