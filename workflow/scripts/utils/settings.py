865e2df97172d345f52c151c2a615391bd8eb54c
Ben Elsworth <be15516@bristol.ac.uk>
2021-01-28 23:17:29 +0000

    moving test to demo

--- workflow/scripts/utils/settings.py
+++ workflow/scripts/utils/settings.py
@@ -8,20 +8,20 @@ env.read_env()
 
 # remove ./ from beginning of neo4j variables to avoid issues with docker
 neo4j_import_dir = re.sub(
-    "^./", "", env("NEO4J_IMPORT_DIR", "./test/neo4j/0.0.1/import")
+    "^./", "", env("NEO4J_IMPORT_DIR", "./demo/neo4j/0.0.1/import")
 )
-neo4j_log_dir = re.sub("^./", "", env("NEO4J_LOG_DIR", "./test/neo4j/0.0.1/logs"))
-neo4j_data_dir = re.sub("^./", "", env("NEO4J_DATA_DIR", "./test/neo4j/0.0.1/data"))
+neo4j_log_dir = re.sub("^./", "", env("NEO4J_LOG_DIR", "./demo/neo4j/0.0.1/logs"))
+neo4j_data_dir = re.sub("^./", "", env("NEO4J_DATA_DIR", "./demo/neo4j/0.0.1/data"))
 
-snakemake_logs = env("SNAKEMAKE_LOGS", "test/results/logs")
-data_dir = env("DATA_DIR", "test/source_data")
-processing_dir = env("PROCESSING_DIR", "test/scripts/processing")
-graph_dir = env("GRAPH_DIR", "test/results/graph_data")
+snakemake_logs = env("SNAKEMAKE_LOGS", "demo/results/logs")
+data_dir = env("DATA_DIR", "demo/source_data")
+processing_dir = env("PROCESSING_DIR", "demo/scripts/processing")
+graph_dir = env("GRAPH_DIR", "demo/results/graph_data")
 neo4j_address = env("NEO4J_ADDRESS", socket.getfqdn())
 server_name = env("SERVER_NAME", None)
 graph_version = env("GRAPH_VERSION", "0.0.1")
 container_name = env("GRAPH_CONTAINER_NAME", "neo4j-pipeline-demo-graph")
-config_path = env("CONFIG_PATH", "test/config")
+config_path = env("CONFIG_PATH", "demo/config")
 threads = env("THREADS", 4)
 
 graph_bolt = env("GRAPH_BOLT_PORT", "7687")
