7ca18db2951f000ccd09ee0e5aaf365458e39b23
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2021-03-24 15:05:13 +0000

    added schema index test

--- tests/graph_database/test_db_schema.py
+++ tests/graph_database/test_db_schema.py
@@ -4,7 +4,7 @@ from loguru import logger
 
 from workflow.scripts.utils.general import neo4j_connect
 
-from tests.utils.load_schema import meta_node_names, meta_rel_names
+from tests.utils.load_schema import meta_node_names, meta_rel_names, meta_node_dict
 
 
 def test_connect():
@@ -24,8 +24,8 @@ def test_meta_node_exist():
         CALL db.labels() YIELD label RETURN label
     """
     driver = neo4j_connect()
-    session = driver.session()
-    data = session.run(query).data()
+    with driver.session() as session:
+        data = session.run(query).data()
     db_meta_node_names = set([_["label"] for _ in data])
     if {"Meta"}.issubset(db_meta_node_names):
         db_meta_node_names.remove({"Meta"})
@@ -41,17 +41,22 @@ def test_meta_rel_exist():
         RETURN relationshipType AS label
     """
     driver = neo4j_connect()
-    session = driver.session()
-    data = session.run(query).data()
+    with driver.session() as session:
+        data = session.run(query).data()
     db_meta_rel_names = set([_["label"] for _ in data])
     logger.info(f"meta_rel_names: {meta_rel_names}")
     logger.info(f"db_meta_rel_names: {db_meta_rel_names}")
     assert set(meta_rel_names) == db_meta_rel_names
 
 
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+def test_meta_node_index():
+    query = "CALL db.indexes()"
+    driver = neo4j_connect()
+    with driver.session() as session:
+        data = session.run(query).data()
+    actual_node_indexes = {(_["labelsOrTypes"][0], _["properties"][0]) for _ in data}
+    expected_node_indexes = {
+        (key, value["index"]) for key, value in meta_node_dict.items()
+    }
+    # We would expect expected_indexes to be a subset of current_indexes
+    assert len(expected_node_indexes.difference(actual_node_indexes)) == 0
